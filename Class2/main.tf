80107ed281c83c6e35bee078f93857ae549be4ba
BohdanMatiuk <mbs.dvp@gmail.com>
2024-10-12 18:10:19 +0000

    add class3

--- Class2/main.tf
+++ Class2/main.tf
@@ -3,5 +3,28 @@ provider aws {
 } 
 resource "aws_key_pair" "deployer" {
   key_name   = "bastion_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpRhJAxN/nd3TaNtSwLJ+7slwsvNIrdM0FVhNxuAiQAf9c55VIv+/SQ7w87ByQMx7V5SEnLijDbap18wbv1TH4gME70SvRQ5zTpeB8ip7AkEkUh0gHEvo6OTLi7TBnJgLI9eq6YtvSm/Fy/j4noXlQ8Lb/TIaAZiK1qJlNzLt5shbK2yz44gO7n/A6Ly4LaUSeYdM/ffJ47XfltaXKICUrwbuP/76I7ryWvCwR2x+WjJeP7OurV2NulfypDLqNNK27jS6cbZ56rdjSPbETieBtyL35guI+dz0fEuzj9/qZpNU8MGHGuChKptbpdvMRSVN2iqFczN2Jhn5XBg+cxKT30JBBXEk2QNn9TBegzl1freumHPrrn+xmxWMGG3YQvL3TfXKf4/OqMEQwYRJICcJtNvfJazxLI4rHd3KTS9HL/MPJGTqV9JAGSAX5Ewi2dqfaayWJtp9h6EyNfry+5abwjmt705d63t+5xUCY/DX8vnGSTA4ZWHnbEPTl430LuJc= ec2-user@ip-172-31-41-91.eu-central-1.compute.internal"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "bucket-bohdan12"
+  force_destroy = true
+ }
+
+ resource "time_sleep" "wait_10_seconds" {
+  depends_on = [aws_s3_bucket.example]
+
+  create_duration = "10s"
+}
+
+ resource "aws_s3_object" "object" {
+  depends_on = [time_sleep.wait_10_seconds]
+  bucket = "bucket-bohdan12"
+  key    = "main.tf"
+  source = "main.tf"
+ }
+
+ resource "aws_s3_bucket" "example2" {
+  bucket_prefix = "bucket-bohdan"
+  force_destroy = true
+ }
---
+++ Class3/local.tf
@@ -0,0 +1,7 @@
+locals {
+    common_tags = {
+        Name = "hello"
+        Env = "dev"
+        Team = "DevOps"
+    }
+}
